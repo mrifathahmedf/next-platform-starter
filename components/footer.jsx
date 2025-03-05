101c1d62cf3df003c276eb8b6c8abe8391a66bfe
mrifathahmedf <mdrifathahmed2020@gmail.com>
2025-03-05 18:52:02 +0600

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,13 +1,11 @@
+// components/Navbar.js
 import Link from 'next/link';
 
-export function Footer() {
-    return (
-        <footer className="pt-16 pb-12 sm:pt-24 sm:pb-16">
-            <p className="text-sm">
-                <Link href="https://docs.netlify.com/frameworks/next-js/overview/" className="underline transition decoration-dashed text-primary underline-offset-8 hover:opacity-80">
-                    Next.js on Netlify
-                </Link>
-            </p>
-        </footer>
-    );
-};
+export default function Navbar() {
+  return (
+    <nav>
+      <Link href="/">হোম</Link> |  
+      <Link href="/about">আমাদের সম্পর্কে</Link>
+    </nav>
+  );
+}
